9fed1672e96dcbf6058a8ea55f7b08ea956db012
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-05-12 08:56:07 +0200

    Creating an interactive website

--- src/main/kotlin/com/example/plugins/Templating.kt
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -1,13 +1,13 @@
 package com.example.plugins
 
 import freemarker.cache.*
-import io.ktor.server.freemarker.*
-import io.ktor.server.response.*
+import freemarker.core.*
 import io.ktor.server.application.*
-import io.ktor.server.routing.*
+import io.ktor.server.freemarker.*
 
 fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
 }
